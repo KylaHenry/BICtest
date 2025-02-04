9e3e44af5cf70859ebb402c21103acca3fc783c0
Kyla Henry <kylachenry15@gmail.com>
2025-02-04 00:14:20 -0500

    Removed traveler greeting from tavern

--- Traveler.py
+++
@@ -1,9 +0,0 @@
-def Greetings(): #defining the function
-    print(f"Welcome traveler, please enter your name to continue on your journey.") #welcome message
-    name = input("Enter your name: ")  # get user input
-    print(f"Hello {name}. Welcome to the magic shop!")  # prints user input
-    grocery =input("Enter the item you are looking to purchase for replenishment:") #get user input
-    print(f"You're lucky. Here's the last {grocery}.") #prints user input
-    print(f"Safe travels on your journey traveler. Come back soon!") #closing message
-
-Greetings()  # Call the function
